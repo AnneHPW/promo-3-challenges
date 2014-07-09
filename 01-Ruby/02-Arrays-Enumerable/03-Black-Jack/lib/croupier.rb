96d10804a5eff0d0dffa46128428adeb5835b823
AnneHPW <annebuffetaud@hotmail.com>
2014-07-09 17:47:54 +0200

    03-Black-Jack

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,17 +1,56 @@
 require_relative 'black_jack'
 
+
 def play_game
-  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+
+  score_player = 0
+  score_bank = bank_score
+
+  while asking_for_card?(score_player) == true
+  score_player += pick_card
+  puts state_of_the_game(score_player, score_bank)
+  end
+
+  total = game_outcome(score_bank, score_player)
+  puts build_message_for(total)
+
 end
 
+  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+
 def state_of_the_game(score, bank)
+  "Your score is #{score}, bank is #{bank}!"
   # TODO: Returns custom message with player's score and bank's score
 end
 
 def asking_for_card?(score)
+
+
+
+  if  score < 21
+  puts "Cards ? (type 'Y' or 'Yes for a new card)"
+  response = gets.chomp
+    if response == "Y" || response == "yes"
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
 end
 
 def build_message_for(outcome)
-  #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+  if outcome[1] > 21
+    "You are over 21... you loose."
+  elsif outcome[1] == 21
+    return "Black Jack!"
+  elsif outcome[1] > outcome[0]
+     "You beat the bank! You win."
+  else
+      "Bank beats you! You loose."
+  end
+ #TODO: return specific message depending on the game outcome (= bank's score and user's score)
 end
