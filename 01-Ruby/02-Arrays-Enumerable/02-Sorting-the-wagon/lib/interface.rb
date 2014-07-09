fb4a6a8245e67f85edf9a17377d0eb86a277a827
AnneHPW <annebuffetaud@hotmail.com>
2014-07-09 14:46:43 +0200

    02-Sorting-the-wagon

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,9 +6,28 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
+  students << name
 
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
 
+students = wagon_sort(students)
+students.delete("")
+effectif = students.size
+
+table_wolast = students.slice(0, effectif-1)
+
+list_wolast = table_wolast.join (", ")
+
+last = students.last.to_s
+
+puts "Congratulations! Your Wagon has #{effectif} students:
+ -  #{list_wolast} and #{last}."
+
+#students = wagon_sort(student)
+#nombre_tableau = students.size
+#return nombre_tableau
+
+#return wagon_sort(students)
 # TODO: Call `wagon_sort` method and display the sorted student list
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,5 @@
+
 def wagon_sort(students)
-  # TODO: return a copy of students, sorted alphabetically
+ return students.sort_by{|word| word.downcase}
 end
+
