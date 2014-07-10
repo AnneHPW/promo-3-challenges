44e39d21a304276607a62951277cecbf63e27068
AnneHPW <annebuffetaud@hotmail.com>
2014-07-10 10:09:12 +0200

    06-Destructive-methods

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,14 @@
 def pmu_format!(race_array)
-  #TODO: modify the given array so that it is PMU-consistent
-end
 
+  race_array.map! do |x|
+    i = race_array.index(x) + 1
+    "#{i}-#{x}!"
+
+  #TODO: modify the given array so that it is PMU-consistent
+  end
+    race_array.reverse!
+  end
+  puts pmu_format!(["Abricot du Laudot", "Black Caviar", "Brigadier Gerard", "Coup de Folie"])
 
 
 
