a407bbbceed674ce431f91cfc9c1cb6195599cb2
AnneHPW <annebuffetaud@hotmail.com>
2014-07-17 18:49:39 +0200

    mail_joke

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,11 +1,21 @@
 # Encoding: utf-8
 
 def mail_joke(email)
-  if# je traite le case de l'erreur
-  else# je traite en case les différents cas de figure
-    case object
-    when condition
-
+  if email.match(/^(\w[-._+\w]*\w@\w[-._\w]*\w\.\w{2,3})$/) == nil
+    raise ArgumentError# je traite le case de l'erreur
+    else
+    match_data = email.match(/(\S+)@(\S+)/)
+    name_mail = match_data[1].to_s
+    mail_com = match_data[2]
+    name_mail.gsub!(/\./, ' ')
+    if mail_com == "lewagon.org"
+        "Well done #{name_mail}, you're skilled and capable"
+    elsif mail_com == "gmail.com"
+        "#{name_mail}, you're an average but modern person"
+    elsif mail_com == "live.com"
+        "#{name_mail}, aren't you born before 1973?"
+    else
+        "Sorry #{name_mail}, we don't know how to judge '#{mail_com}'"
     end
   end# TODO: Return a joke suited to the email provided
 end
