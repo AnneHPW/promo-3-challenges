1344b6e17bef382e912c7c72d641a4712ce02107
AnneHPW <annebuffetaud@hotmail.com>
2014-07-17 14:41:56 +0200

    restaurant

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,34 @@
 class Restaurant
-  
-  #TODO add relevant accessors if necessary
-  
+
+  attr_accessor :city, :name, :average_rating, :restaurants#TODO add relevant accessors if necessary
+
   def initialize(city, name)
+    @name = name
+    @city = city
+    @average_rating = 0
+    @rates = []
     #TODO: implement constructor with relevant instance variables
   end
-  
+
+  def rate(grade)
+    sum = 0
+    @rates << grade
+    @rates.each do |rate|
+      sum = rate + sum
+    end
+    @average_rating = sum / @rates.size
+  end
+
+
+  def self.filter_by_city(restaurants, city)
+    resto_selection = []
+    restaurants.each do |resto|
+      if resto.city == city
+        resto_selection << resto
+      end
+    end
+    resto_selection
+  end
+
   #TODO: implement #filter_by_city and #rate methods
-  
 end
