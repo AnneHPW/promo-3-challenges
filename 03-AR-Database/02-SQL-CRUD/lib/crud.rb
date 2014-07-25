9210c17e2e79dff5f149700e6d24da18b39c9cb4
AnneHPW <annebuffetaud@hotmail.com>
2014-07-25 14:51:35 +0200

    crud

--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -2,31 +2,46 @@ require 'sqlite3'
 
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{ CREATE TABLE IF NOT EXISTS blog
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(100),
+      rating INT,
+      source_url VARCHAR(255),
+      date DATE)
+  }  # %q allows for multiline string in ruby
   db.execute(sql)
 end
 
 def create_post(db, post)
-  #TODO: add a new post to your db
+  querie_new_post = ("INSERT INTO blog (name, rating, source_url, date)
+              VALUES ('#{post[:name]}', '#{post[:rating]}', '#{post[:source_url]}', '#{post[:date]}')")
+  #display_new_post = ("SELECT * FROM blog ORDER BY ID DESC LIMIT 1")
+  db.execute(querie_new_post)
+  #db.execute(display_new_post)
+
 end
 
 def get_posts(db)
+  db.execute("SELECT * FROM blog")
    #TODO: list all posts
 end
 
 def get_post(db, id)
-  #TODO: get a specific post
+  db.execute("SELECT * FROM blog WHERE id = #{id}")
 end
 
 def update_post(db, id, name)
-	#TODO: update a post's name in your db
+  db.execute("UPDATE blog SET name = '#{name}' WHERE id = #{id}")
+  #TODO: update a post's name in your db
 end
 
 def delete_posts(db)
-  #TODO: delete a post in your db
+    db.execute("DELETE FROM blog")
 end
 
 def delete_post(db, id)
+  db.execute("DELETE FROM blog
+            WHERE id = #{id}")
   #TODO: delete a specific post in your db
 end
+
--- 03-AR-Database/02-SQL-CRUD/lib/interface.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/interface.rb
@@ -5,10 +5,17 @@ def ask_and_get(param)
   gets.chomp
 end
 
+
+def get_post_id
+  puts "Please enter post id : "
+  print "> "
+  gets.chomp.to_i
+end
+
 db_path = "db/news.sqlite"
 db = SQLite3::Database.new(db_path)
 
-create_scheme(db) 
+create_scheme(db)
 
 while true
 
@@ -16,24 +23,50 @@ while true
   puts "1. Create a post"
   puts "2. Read your posts"
   puts "3. Delete all posts"
-  puts "4. Exit"
-  
+  puts "4. Read a post"
+  puts "5. Change the title of one post"
+  puts "6. Delete a post"
+  puts "7. Exit"
+
   # TODO: add other CRUD tasks if you wish!
 	choice =  gets.chomp.to_i
-	
+
 	case choice
-  when 1
-    name = ask_and_get("name")
-    source_url = ask_and_get("source url")
-    rating = ask_and_get("rating")
-    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
-    create_post(db, post)
-  when 2
-    get_posts(db)
-  when 3
-    delete_posts(db)
-  when 4 
+    when 1
+      name = ask_and_get("name")
+      source_url = ask_and_get("source url")
+      rating = ask_and_get("rating")
+      post = { name: name, source_url: source_url, date: Time.now, rating: rating }
+      puts create_post(db, post)
+    when 2
+      posts = get_posts(db)
+      posts.each do |post|
+        puts
+        puts "==============="
+        puts "Id: #{post[0]}"
+        puts "Name: #{post[1]}"
+        puts "Source URL: #{post[2]}"
+        puts "Date: #{post[3]}"
+        puts "==============="
+        puts
+      end
+    when 3
+      delete_posts(db)
+    when 4
+      id = get_post_id
+      puts get_post(db, id)
+    when 5
+        id = get_post_id
+        puts "Please enter the new title : "
+        print "> "
+        name = gets.chomp
+        update_post(db, id, name)
+    when 6
+        id = get_post_id
+        delete_post(db, id)
+    when 7
+      exit()
     break
-	end 
-	
+	end
+
 end
