c09f40001e0a72d0ff50ae6f9625480d080ec0c5
AnneHPW <annebuffetaud@hotmail.com>
2014-08-04 16:40:45 +0200

    application.js slide

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,3 +1,12 @@
+$("#message").hide()
+$(document).ready(function() {
+  $("#message").slideDown(); // executed when the document is ready!
+});
+
+$( "#message" ).click(function() {
+  $( this ).slideUp();
+});
+  // Run code
 /*
   *
   * TODO: add your code here!
